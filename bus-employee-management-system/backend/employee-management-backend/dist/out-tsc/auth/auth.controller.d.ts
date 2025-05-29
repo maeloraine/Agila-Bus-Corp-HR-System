7e386fd958403f54e793de6fef688edf3d19d854
Carl Bergado <115414567+Carts1024@users.noreply.github.com>
2025-05-29 13:58:04 +0800

    Delete bus-employee-management-system/backend/employee-management-backend directory

--- bus-employee-management-system/backend/employee-management-backend/dist/out-tsc/auth/auth.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class AuthController {
-    private readonly httpService;
-    constructor(httpService: HttpService);
-    login(credentials: any): Promise<any>;
-}
